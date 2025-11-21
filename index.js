9ac3b3e12f7e5bcce7bd3b85c29b76270c2395de
Nikhil Choudhary <nikhilchoudhary2242@gmail.com>
2025-11-21 10:38:37 +0530

    update = method

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ arr.forEach(button =>{
     button.addEventListener('click',(e) =>{
         if(e.target.innerHTML == '='){
             string = eval(string);
-            input.valu = string;
+            input.value = string;
         }
 
         else if(e.target.innerHTML == 'Ac'){
             string ="";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
             input.value = string;
         }
         else{
@@ -21,5 +25,4 @@ arr.forEach(button =>{
         
         
     })
-    console.log("nikhil");
 })
